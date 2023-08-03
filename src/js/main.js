9cc79cb309003c1f6f72ad7416155e4da5c85eb4
mihainsto <mihaistoianro@gmail.com>
2023-08-03 16:26:54 +0300

    add files

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+// Load Styles
+import '../scss/main.scss';
+
+// Load Bootstrap init
+import {initBootstrap} from "./bootstrap.js";
+
+
+// Loading bootstrap with optional features
+initBootstrap({
+  tooltip: true,
+  popover: true,
+  toasts: true,
+});
+
+
+
+// Simple js method to provide drag scroll to symptoms container
+// Can be replaced with better npm package
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+const ele = document.getElementById('drag-scroll-container');
+
+
+const mouseMoveHandler = function (e) {
+  // How far the mouse has been moved
+  const dx = e.clientX - pos.x;
+  const dy = e.clientY - pos.y;
+
+  // Scroll the element
+  ele.scrollTop = pos.top - dy;
+  ele.scrollLeft = pos.left - dx;
+};
+
+const mouseUpHandler = function () {
+  document.removeEventListener('mousemove', mouseMoveHandler);
+  document.removeEventListener('mouseup', mouseUpHandler);
+
+  ele.style.cursor = 'grab';
+  ele.style.removeProperty('user-select');
+};
+
+document.getElementById('drag-scroll-container').onmousedown=function (e) {
+  pos = {
+      // The current scroll
+      left: ele.scrollLeft,
+      top: ele.scrollTop,
+      // Get the current mouse position
+      x: e.clientX,
+      y: e.clientY,
+  };
+
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
+};
